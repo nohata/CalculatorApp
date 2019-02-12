59c6751f22b82bb672173eea78c6c3207a571ad6
野畑 悠典 <yu----qqq@i.softbank.jp>
2019-02-12 18:33:06 +0900

    NextView追加、icon追加

---
+++ CalculatorApp/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  CalculatorApp
+//
+//  Created by Yusuke Nohata on 2019/02/12.
+//  Copyright © 2019年 yusuke nohata. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
